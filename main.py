6fb213044131b73f34239978378614f127872b45
Yarashev Jonibek <yarashevjonibek@gmail.com>
2026-07-30 13:44:19 +0500

    done

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 word = input()
+word_lower = word.lower()
 vowels = ['a', 'e', 'i', 'o', 'u']
 count = 0
 
-for letter in word:
+for letter in word_lower:
     if letter in vowels:
         count += 1
 
